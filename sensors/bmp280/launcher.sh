7cff91f7605ef1677e9624da120a66f8109e3ffc
Melvin Campos Casares <melvin.campos@outlook.be>
2020-02-05 00:40:07 +0100

    Add first run and launcher runnable scripts

---
+++ sensors/bmp280/launcher.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#This script is used to automaticaly execute the script bmp280.py.
+#The bmp280 will connect the BMP sensor and the RaspBerry Pi.
+
+cd /PiSense/sensors/bmp280
+sudo python3 bmp280.py
