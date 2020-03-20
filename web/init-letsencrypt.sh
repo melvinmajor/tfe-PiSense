05cae4dc25c030ecbe4652cf9caff81f8e681b73
Melvin Campos Casares <melvin.campos@outlook.be>
2020-03-21 00:46:07 +0100

    Temporarily revert to HTTP only

--- web/init-letsencrypt.sh
+++ web/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(camposcasares.be www.camposcasares.be)
+domains=(camposcasares.be)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="m.camposcasares@students.ephec.be" # Adding a valid address is strongly recommended
