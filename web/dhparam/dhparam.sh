629cc3717877a1e12a380f855881111430c3a552
Melvin Campos Casares <melvin.campos@outlook.be>
2020-03-23 17:12:44 +0100

    Let's Encrypt SSL certificate (production) fully functional

---
+++ web/dhparam/dhparam.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo openssl dhparam -out dhparam-2048.pem 2048
+
--- web/prod-letsencrypt.sh
+++ web/prod-letsencrypt.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-docker run -it --rm \
+sudo docker run -it --rm \
     -v /docker-volumes/etc/letsencrypt:/etc/letsencrypt \
     -v /docker-volumes/var/lib/letsencrypt:/var/lib/letsencrypt \
     -v /home/mcc/git/tfe-PiSense/web/www:/data/letsencrypt \
     -v "/docker-volumes/var/log/letsencrypt:/var/log/letsencrypt" \
     certbot/certbot \
     certonly --webroot \
-    --register-unsafely-without-email --agree-tos \
+    --email m.camposcasares@students.ephec.be --agree-tos --no-eff-email \
     --webroot-path=/data/letsencrypt \
-    --staging \
     -d camposcasares.be -d www.camposcasares.be
