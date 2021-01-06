a352378b0465f200e179d039b18bd9ec74a872f0
Melvin Campos Casares <melvin.campos@outlook.be>
2021-01-06 23:43:56 +0100

    SDS011 sensor - small code optimisation

--- sensors/sds011/sds011sensor.py
+++ sensors/sds011/sds011sensor.py
@@ -26,7 +26,7 @@ config.read('config.ini')
 
 ''' default variables values '''
 default_api_url = config['default']['default_api_url']
-sending_timeout = int(config['default']['sending_timeout']) # timeout used to wait a certain amount of time before returning the get/post of API
+sending_timeout = int(config['default']['sending_timeout'])  # timeout used to wait a certain amount of time before returning the get/post of API
 default_time = int(config['aqi']['default_time'])
 localhost_usage = config.getboolean('default', 'localhost_usage')
 api_usage = config.getboolean('default', 'api_usage')
@@ -115,6 +115,7 @@ print('-------------------')
 # The sensor will need a moment to gather initial readings
 time.sleep(2)
 
+
 ''' Method which may be used to check if parameter used has a value that can be considered as boolean '''
 def str2bool(v):
     if isinstance(v, bool):
@@ -126,10 +127,12 @@ def str2bool(v):
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
 
+
 ''' ISO8601 format date and time'''
 def get_date_time():
-#    return now.replace(tzinfo=datetime.timezone(offset=utc_offset)).isoformat()
-     return datetime.now().strftime('%Y-%m-%dT%H:%M:%S:%SZ')
+    # return now.replace(tzinfo=datetime.timezone(offset=utc_offset)).isoformat()
+    return datetime.now().strftime('%Y-%m-%dT%H:%M:%S:%SZ')
+
 
 ser = serial.Serial()
 ser.port = "/dev/ttyUSB0"
@@ -140,8 +143,10 @@ ser.flushInput()
 
 byte, data = 0, ""
 
+
 def dump(d, prefix=''):
     logger.info(prefix + ' '.join(x.encode('hex') for x in d))
+
 
 def construct_command(cmd, data=[]):
     assert len(data) <= 12
@@ -155,18 +160,21 @@ def construct_command(cmd, data=[]):
         dump(ret, '> ')
     return ret
 
+
 def process_data(d):
     r = struct.unpack('<HHxxBB', d[2:])
     pm25 = r[0]/10.0
     pm10 = r[1]/10.0
     checksum = sum(ord(v) for v in d[2:8])%256
     return [pm25, pm10]
-    #print("PM 2.5: {} μg/m^3  PM 10: {} μg/m^3 CRC={}".format(pm25, pm10, "OK" if (checksum==r[2] and r[3]==0xab) else "NOK"))
+    # print("PM 2.5: {} μg/m^3  PM 10: {} μg/m^3 CRC={}".format(pm25, pm10, "OK" if (checksum==r[2] and r[3]==0xab) else "NOK"))
+
 
 def process_version(d):
     r = struct.unpack('<BBBHBB', d[3:])
     checksum = sum(ord(v) for v in d[2:8])%256
-    logger.info("Y: {}, M: {}, D: {}, ID: {}, CRC={}".format(r[0], r[1], r[2], hex(r[3]), "OK" if (checksum==r[4] and r[5]==0xab) else "NOK"))
+    logger.info("Y: {}, M: {}, D: {}, ID: {}, CRC={}".format(r[0], r[1], r[2], hex(r[3]), "OK" if (checksum == r[4] and r[5] == 0xab) else "NOK"))
+
 
 def read_response():
     byte = 0
@@ -179,9 +187,11 @@ def read_response():
         dump(d, '< ')
     return byte + d
 
+
 def cmd_set_mode(mode=MODE_QUERY):
     ser.write(construct_command(CMD_MODE, [0x1, mode]))
     read_response()
+
 
 def cmd_query_data():
     ser.write(construct_command(CMD_QUERY_DATA))
@@ -191,25 +201,30 @@ def cmd_query_data():
         values = process_data(d)
     return values
 
+
 def cmd_set_sleep(sleep):
     mode = 0 if sleep else 1
     ser.write(construct_command(CMD_SLEEP, [0x1, mode]))
     read_response()
 
+
 def cmd_set_working_period(period):
     ser.write(construct_command(CMD_WORKING_PERIOD, [0x1, period]))
     read_response()
+
 
 def cmd_firmware_ver():
     ser.write(construct_command(CMD_FIRMWARE))
     d = read_response()
     process_version(d)
 
+
 def cmd_set_id(id):
     id_h = (id>>8) % 256
     id_l = id % 256
     ser.write(construct_command(CMD_DEVICE_ID, [0]*10+[id_l, id_h]))
     read_response()
+
 
 def pub_mqtt(jsonrow):
     cmd = ['mosquitto_pub', '-h', MQTT_HOST, '-t', MQTT_TOPIC, '-s']
@@ -219,13 +234,13 @@ def pub_mqtt(jsonrow):
 
 
 ''' JSON part '''
-## {"datetime": "2020-05-24T15:08:05.274635+02:00", "pm25": 2.3, "pm10": 4.9}
+# {"datetime": "2020-05-24T15:08:05.274635+02:00", "pm25": 2.3, "pm10": 4.9}
 def sensor_to_json():
     # dict which will be used by JSON
-    dave = {'datetime': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), # date T time in ISO8601
-            'PM2': values[0], # PM2.5 in µg/m3
-            'PM10': values[1]} # PM10 in µg/m3
-    #date_json=dave
+    dave = {'datetime': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),  # date T time in ISO8601
+            'PM2': values[0],  # PM2.5 in µg/m3
+            'PM10': values[1]}  # PM10 in µg/m3
+    # date_json=dave
     # This part is for debug mode only because when used, it stop the sending of JSON to API
     # data_json = json.dumps(dave)
     # logger.info('Records: %s', dave)
@@ -235,21 +250,23 @@ def sensor_to_json():
     #    f.write(data_json + "\n")
     return dave
 
+
 ''' Fail method '''
 def fail(msg):
     print(">>> Oops: ",msg,file=sys.stderr)
     logger.warning('Oops: %s', msg)
 
+
 def post_data(datas):
     logger.info('Sending data to server via API...')
     sensor_to_json()
     try:
-        #print('debug')
-        #print(datas)
-        #print(args.url)
+        # print('debug')
+        # print(datas)
+        # print(args.url)
         r = requests.post(args.url,data=datas)
         # To see what the online server answer:        print(r.text)
-        #logger.info('Status code: %s - %s', str(r.status_code), r.json()['message'])
+        # logger.info('Status code: %s - %s', str(r.status_code), r.json()['message'])
         if r.status_code in range(200,300):
             logger.info('Success')
         else:
@@ -265,6 +282,7 @@ def post_data(datas):
         # catastrophic error, you need to go to jail
         fail('Request error')
 
+
 def local_data(datas):
     logger.info('Writing data to localhost webserver...')
     sensor_to_json()
@@ -274,7 +292,7 @@ def local_data(datas):
     # save it
     try:
         with open(JSON_FILE, 'w') as outfile:
-            #json.dump(data, outfile)
+            # json.dump(data, outfile)
             outfile.write(dave)
             logger.info('Data recorded successfully')
         if MQTT_HOST != '':
@@ -282,71 +300,69 @@ def local_data(datas):
     except IOError as e:
         fail('IOError while trying to open and write JSON file')
 
+
 def notification(dataType, info):
     base_url = 'https://maker.ifttt.com/trigger/{}/with/key/{}'
     url = base_url.format(EVENT_NAME, KEY)
-    report = {}
-    report["value1"] = dataType
-    report["value2"] = str(info)
-    report["value3"] = PISENSE_ALERT_NOTIFICATION
+    report = {"value1": dataType, "value2": str(info), "value3": PISENSE_ALERT_NOTIFICATION}
     requests.post(url, data=report)
 
 
 if __name__ == "__main__":
     cmd_set_sleep(0)
-    #print('1')
+    # print('1')
     cmd_set_mode(1)
     cmd_firmware_ver()
-    #print('2')
+    # print('2')
     cmd_set_working_period(PERIOD_CONTINUOUS)
     cmd_set_mode(MODE_QUERY);
     while True:
         try:
             cmd_set_sleep(0)
-            
-            now = datetime.datetime.now() # Get current date and time
+
+            now = datetime.datetime.now()  # Get current date and time
             utc_offset_sec = time.altzone if time.localtime().tm_isdst else time.timezone
             utc_offset = datetime.timedelta(seconds=-utc_offset_sec)
 
             for t in range(15):
                 values = cmd_query_data();
                 if values is not None and len(values) == 2:
-                  print("PM2.5: ", values[0], ", PM10: ", values[1])
-                  time.sleep(2)
+                    print("PM2.5: ", values[0], ", PM10: ", values[1])
+                    time.sleep(2)
 
             data = sensor_to_json()
             # Check if API parameter is used in order to use both localhost and API version or not
-            if(api_usage == True):
+            if api_usage:
                 local_data(data)
                 post_data(data)
-            elif(localhost_usage == True):
+            elif localhost_usage:
                 local_data(data)
             else:
                 fail("Please use `python3 sds011sensor.py` or `python3 sds011sensor.py --api` in order to choose between localhost or API + localhost version...")
 
-            #print("Going to sleep for 15 min...")
+            # print("Going to sleep for 15 min...")
             cmd_set_sleep(1)
-            #time.sleep(900)
+            # time.sleep(900)
 
-            pm25 = values[0] # PM2.5 in µg/m3
-            pm10 = values[1] # PM10 in µg/m3
+            pm25 = values[0]  # PM2.5 in µg/m3
+            pm10 = values[1]  # PM10 in µg/m3
 
             if pm25 >= PM25_HAZARDOUS_CHECKUP:
                 dataType = "AQI PM2.5"
                 info = "hazardous/severely polluted"
                 notification(dataType, info)
                 logger.info('Rich notification sent to IFTTT, %s reached %s', dataType, info)
-            elif pm25 >= PM25_VERYUNHEALTHY_CHECKUP and pm25 < PM25_HAZARDOUS_CHECKUP:
+            elif PM25_VERYUNHEALTHY_CHECKUP <= pm25 < PM25_HAZARDOUS_CHECKUP:
                 dataType = "AQI PM2.5"
                 info = "very unhealthy"
                 notification(dataType, info)
                 logger.info('Rich notification sent to IFTTT, %s reached %s', dataType, info)
-            elif pm25 >= PM25_UNHEALTHY_CHECKUP and pm25 < PM25_VERYUNHEALTHY_CHECKUP:
+            elif PM25_UNHEALTHY_CHECKUP <= pm25 < PM25_VERYUNHEALTHY_CHECKUP:
                 dataType = "AQI PM2.5"
                 info = "unhealthy!"
                 notification(dataType, info)
                 logger.info('Rich notification sent to IFTTT, %s reached %s', dataType, info)
-            elif pm25 >= PM25_SENSITIVE_CHECKUP and pm25 < PM25_UNHEALTHY_CHECKUP:
+            elif PM25_SENSITIVE_CHECKUP <= pm25 < PM25_UNHEALTHY_CHECKUP:
                 dataType = "AQI PM2.5"
                 info = "unhealthy for sensitive groups"
                 notification(dataType, info)
@@ -357,17 +373,17 @@ if __name__ == "__main__":
                 info = "hazardous/severely polluted"
                 notification(dataType, info)
                 logger.info('Rich notification sent to IFTTT, %s reached %s', dataType, info)
-            elif pm10 >= PM10_VERYUNHEALTHY_CHECKUP and pm10 < PM10_HAZARDOUS_CHECKUP:
+            elif PM10_VERYUNHEALTHY_CHECKUP <= pm10 < PM10_HAZARDOUS_CHECKUP:
                 dataType = "AQI PM10"
                 info = "very unhealthy"
                 notification(dataType, info)
                 logger.info('Rich notification sent to IFTTT, %s reached %s', dataType, info)
-            elif pm10 >= PM10_UNHEALTHY_CHECKUP and pm10 < PM10_VERYUNHEALTHY_CHECKUP:
+            elif PM10_UNHEALTHY_CHECKUP <= pm10 < PM10_VERYUNHEALTHY_CHECKUP:
                 dataType = "AQI PM10"
                 info = "unhealthy"
                 notification(dataType, info)
                 logger.info('Rich notification sent to IFTTT, %s reached %s', dataType, info)
-            elif pm10 >= PM10_SENSITIVE_CHECKUP and pm10 < PM10_UNHEALTHY_CHECKUP:
+            elif PM10_SENSITIVE_CHECKUP <= pm10 < PM10_UNHEALTHY_CHECKUP:
                 dataType = "AQI PM10"
                 info = "unhealthy for sensitive groups"
                 notification(dataType, info)
@@ -378,4 +394,3 @@ if __name__ == "__main__":
         except (KeyboardInterrupt, SystemExit):
             logger.info('KeyboardInterrupt/SystemExit caught')
             sys.exit()
-
