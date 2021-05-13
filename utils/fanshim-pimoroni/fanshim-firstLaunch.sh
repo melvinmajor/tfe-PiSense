04ecfd8864621359fbd3c590deca1b4f24f82992
Melvin Campos Casares <melvin.campos@outlook.be>
2021-05-13 18:42:06 +0200

    Change fanshim scripts with preempt
    - Threshold on max temperature 65°C in stead of 60°C
    - Preempt flag: preemptively kick in the fan when CPU frequency raised

--- utils/fanshim-pimoroni/fanshim-firstLaunch.sh
+++ utils/fanshim-pimoroni/fanshim-firstLaunch.sh
@@ -7,4 +7,4 @@ git clone https://github.com/pimoroni/fanshim-python.git ~/Git/fanshim-python
 cd ~/Git/fanshim-python
 sudo ./install.sh
 cd ~/Git/fanshim-python/examples
-sudo ./install-service.sh --on-threshold 60 --off-threshold 50 --delay 2 --brightness 64
+sudo ./install-service.sh --on-threshold 65 --off-threshold 50 --delay 2 --brightness 64 --preempt
--- utils/fanshim-pimoroni/fanshim-launcher.sh
+++ utils/fanshim-pimoroni/fanshim-launcher.sh
@@ -3,4 +3,4 @@
 cd ~/Git/fanshim-python
 git pull
 cd ~/Git/fanshim-python/examples
-sudo ./install-service.sh --on-threshold 60 --off-threshold 50 --delay 2 --brightness 64
+sudo ./install-service.sh --on-threshold 65 --off-threshold 50 --delay 2 --brightness 64 --preempt
